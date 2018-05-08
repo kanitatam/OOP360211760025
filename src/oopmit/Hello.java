9ed8fbed04b0ed284d1dc982c6ffdcf909beb41e
kanitatam <peedee3016@gmail.com>
2018-05-08 15:16:48 +0700

    delete data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -7,6 +7,6 @@ public class Hello {
        System.out.println("Hello, My name is kanita");
         System.out.println("Major: Information System");
         System.out.println("Faculty: Management Technology");
-        System.out.println("University: RMUTSV");
+
     }//main
 }//class
