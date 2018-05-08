325215658b9f6deae7d6dc5a9854d44864bb6240
kanitatam <peedee3016@gmail.com>
2018-05-08 15:13:44 +0700

    update data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -1,7 +1,12 @@
 package oopmit;
 
+import java.util.concurrent.SynchronousQueue;
+
 public class Hello {
     public static void main(String[] args) {
        System.out.println("Hello, My name is kanita");
+        System.out.println("Major: Information System");
+        System.out.println("Faculty: Management Technology");
+        System.out.println("University: RMUTSV");
     }//main
 }//class
